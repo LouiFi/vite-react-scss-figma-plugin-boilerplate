09e1216391540686657c43f691671f34133fd986
LouiFi <louis@popoyoko.fr>
2024-02-16 05:06:33 +0100

    🐛 Fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.scss";
 
-function App() {
-  const [count, setCount] = useState(0);
+const sayHello = () => {
+  console.log(parent);
+  parent.postMessage({ pluginMessage: { type: "sayHello" } }, "*");
+};
 
+const App = () => {
   return (
     <>
       <div>
@@ -14,16 +16,14 @@ function App() {
       </div>
       <h1>Figma Plugin boilerplate</h1>
       <h2>Vite + React + SCSS - Figma Plugin boilerplate</h2>
+      <button onClick={() => sayHello()}>Test</button>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.tsx</code> and run bun run build
         </p>
       </div>
     </>
   );
-}
+};
 
 export default App;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 figma.showUI(__html__);
 
 figma.ui.resize(500, 500);
+
+figma.notify("Plugin started");
+
+figma.ui.onmessage = (message) => {
+  if (message.type === "sayHello") {
+    figma.notify("Hello");
+  }
+};
