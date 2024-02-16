5604eda6e9d3b748dc119d8c0c37a7056cc39f36
Louis Dauvergne <louis@popoyoko.fr>
2024-02-16 10:28:32 +0100

    🔖 Release 0.1.0

--- scripts/vite/vite-compile-figma-plugin.ts
+++ scripts/vite/vite-compile-figma-plugin.ts
@@ -1,13 +1,15 @@
 import { Plugin } from "vite";
 
 const compileFigmaPlugin = (entrypoints: string[]): Plugin => {
+  console.log(entrypoints);
+
   return {
     name: "vite-compile-figma-plugin",
 
     async buildEnd() {
       await Bun.build({
         entrypoints: entrypoints,
-        outdir: "./dist",
+        outdir: "./src/dist",
       });
     },
   };
